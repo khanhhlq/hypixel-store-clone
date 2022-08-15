c9a43f9bcd1b54fd08e0a4d3103fe4f2977374d8
ooAkira <lee.quockhanh1107@gmail.com>
2022-08-15 23:40:34 +0700

    new folder: hypixel-store-clone

---
+++ js/newNav.js
@@ -0,0 +1,49 @@
+let newNav = {
+    data:[
+        {
+            id: 1,
+            title: "Ranks",
+            icon: ""
+        },
+        {
+            id: 2,
+            title: "Bundles",
+            icon: ""
+        }, 
+        {
+            id: 3,
+            title: "Gold",
+            icon: ""
+        }, 
+        {
+            id: 4,
+            title: "Boosters",
+            icon: ""
+        }, 
+        {
+            id: 5,
+            title: "Mystery Boxes",
+            icon: ""
+        }, 
+        {
+            id: 6,
+            title: "Companions",
+            icon: ""
+        }, 
+        {
+            id: 7,
+            title: "Skyblock Gems",
+            icon: ""
+        }, 
+        {
+            id: 8,
+            title: "Loot Chests",
+            icon: ""
+        }, 
+        {
+            id: 9,
+            title: "Gifts",
+            icon: ""
+        },  
+    ]
+}
